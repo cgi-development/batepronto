1498e1b2363ae434c2d7444d43aa4d0a4c6ede46
democrito88 <d_who@hotmail.com>
2023-08-16 10:11:06 -0300

    adição de cidades de acordo com o estado

--- js/formulario-3.js
+++ js/formulario-3.js
@@ -1,9 +1,21 @@
 $(() => {
-    fetch('../json/estados.json')
+    fetch('./json/estados.json')
     .then((response) => response.json())
     .then((estados) => estados.forEach(estado => 
-        $("#estado").append(`<option>${estado.Nome}</option>`)
+        $("#estado").append(`<option value="${estado.ID}">${estado.Nome}</option>`)
         ));
+    
+    $("#estadoVeiculo").change((this) => {
+        console.log(this.value);
+        fetch('./json/cidades.json')
+        .then(response => response.json())
+        .then(cidades =>{
+            cidades
+            .filter(cidade => cidade.Estado === this.value)
+            .forEach(cidade => 
+                $("#municipioVeiculo").append(`<option value="${cidade.ID}">${cidade.Nome}</option>`))
+        })
+    });
     
     $("#fomulario3").submit((e) => {
         if($("input[name=cep]").prop('value', '')
