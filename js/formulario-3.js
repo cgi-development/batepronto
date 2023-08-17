b6b7843ecb7b22db3ee44bdbbc1f12fb6f846594
democrito88 <d_who@hotmail.com>
2023-08-17 15:40:16 -0300

    17-08-2023

--- js/formulario-3.js
+++ js/formulario-3.js
@@ -1,33 +1,33 @@
 $(() => {
     $("#estado").on("change", () => {
         fetch('./json/cidades.json')
-        .then(response => response.json())
-        .then(cidades =>{
-            $("#municipio").html("");
-            cidades
-            .filter(cidade => cidade.Estado === $("#estado").prop("value"))
-            .forEach(cidade => 
-                $("#municipio").append(`<option value="${cidade.ID}">${cidade.Nome}</option>`))
-        })
+            .then(response => response.json())
+            .then(cidades => {
+                $("#municipio").html("");
+                cidades
+                    .filter(cidade => cidade.Estado === $("#estado").prop("value"))
+                    .forEach(cidade =>
+                        $("#municipio").append(`<option value="${cidade.ID}">${cidade.Nome}</option>`))
+            })
     });
 
     $("#estadoPessoa").on("change", () => {
         fetch('./json/cidades.json')
-        .then(response => response.json())
-        .then(cidades =>{
-            $("#municipioPessoa").html("");
-            cidades
-            .filter(cidade => cidade.Estado === $("#estadoPessoa").prop("value"))
-            .forEach(cidade => 
-                $("#municipioPessoa").append(`<option value="${cidade.ID}">${cidade.Nome}</option>`))
-        })
+            .then(response => response.json())
+            .then(cidades => {
+                $("#municipioPessoa").html("");
+                cidades
+                    .filter(cidade => cidade.Estado === $("#estadoPessoa").prop("value"))
+                    .forEach(cidade =>
+                        $("#municipioPessoa").append(`<option value="${cidade.ID}">${cidade.Nome}</option>`))
+            })
     });
-    
+
     $("#fomulario3").submit((e) => {
-        if($("input[name=cep]").prop('value', '')
-        || $("input[name=cep]").prop('data-valido', 'false')
-        || $("input[name=logradouro]").prop('value', '')
-        || $("input[name=bairro]").prop('value', '')){
+        if ($("input[name=cep]").prop('value', '')
+            || $("input[name=cep]").prop('data-valido', 'false')
+            || $("input[name=logradouro]").prop('value', '')
+            || $("input[name=bairro]").prop('value', '')) {
             e.preventDefault();
             window.alert("Por favor, preencha os campos obrigatórios");
             $("input[name=cep]").attr("style", "border-color: red");
@@ -90,9 +90,9 @@ $(() => {
 
         dados.forEach((value, key) => {
             console.log([key, value]);
-            if(key === "anexoArquivo"){
+            if (key === "anexoArquivo") {
                 dadosFormatados[key] = value.name;
-            }else{
+            } else {
                 dadosFormatados[key] = value;
             }
         });
@@ -105,16 +105,57 @@ $(() => {
     });
 
     fetch('./json/estados.json')
-    .then((response) => response.json())
-    .then((estados) => estados.forEach(estado => {
+        .then((response) => response.json())
+        .then((estados) => estados.forEach(estado => {
             $(".estado").append(`<option value="${estado.ID}">${estado.Nome}</option>`)
         }
-    ));
-    
+        ));
+
+    $("#formSinistro").submit((e) => {
+        e.preventDefault();
+        let dados = new FormData(e.target);
+        
+        let contadorenvolvidos = 0;
+        $("#envolvidos").children().toArray().forEach(
+            (linha, key) => {
+                let obj = { nome: "", cpf: "", habilitacao: "", tipo: "" }
+                obj.nome = $(linha).find('td:eq(0)').html();
+                obj.cpf = $(linha).find('td:eq(1)').html();
+                obj.habilitacao = $(linha).find('td:eq(2)').html();
+                obj.tipo = $(linha).find('td:eq(3)').html();
+                dados.append(`envolvidos${contadorenvolvidos}`, obj);
+                contadorenvolvidos++;
+            }
+        )
+        let contadorveiculos = 0;
+        $("#veiculos").children().toArray().forEach(
+            (linha) => {
+                let obj = { placa: "", tipo: "", caracteristica: ""}
+                obj.placa = $(linha).find('td:eq(0)').html();
+                obj.tipo = $(linha).find('td:eq(1)').html();
+                obj.caracteristica = $(linha).find('td:eq(2)').html();
+                dados.append(`veiculos${contadorveiculos}`, obj);
+                contadorveiculos++;
+            }
+        )
+        let contadoranexos = 0;
+        $("#anexos").children().toArray().forEach(
+            (linha) => {
+                let obj = { tipo: "", anexos: ""}
+                obj.tipo = $(linha).find('td:eq(0)').html();
+                obj.anexos = $(linha).find('td:eq(1)').html();
+                dados.append(`anexos${contadoranexos}`, obj);
+                contadoranexos++;
+            }
+        )
+        console.log(dados);
+    })
+
 });
 
-function contaCaracteresRestantes(idTextarea, idSpan){
+function contaCaracteresRestantes(idTextarea, idSpan) {
     const total = 32700;
     let restante = (total - $(idTextarea).prop("value").length);
     $(idSpan).html(restante);
 }
+
