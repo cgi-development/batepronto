b8f1b1db14a6a603502164b5a28692137ee4bb89
democrito88 <d_who@hotmail.com>
2024-12-09 10:24:50 -0300

    09-12-2024

--- src/Componentes/IndicePagina/index.js
+++ src/Componentes/IndicePagina/index.js
@@ -1,21 +1,34 @@
-import { Button, Card, Container, Stack } from "react-bootstrap";
+import { Card, Container } from "react-bootstrap";
+import { TiDocumentText } from "react-icons/ti";
+import { FaListCheck, FaRegThumbsUp } from "react-icons/fa6";
+import { RiSpeakLine } from "react-icons/ri";
 import "./IndicePagina.css";
 
 const IndicePagina = ({indice}) => {
-    const indices = ["", "secondary","secondary","secondary","secondary"];
-    indices[indice] = "primary";
+    //const indices = ["", "secondary","secondary","secondary","secondary"];
+    //indices[indice] = "primary";
 
     return(
         <Container>
             <Card>
                 <Card.Body>
                     <div className="form-etapas">
-                        <Stack direction="horizontal" gap={2}>
-                        <div className="p-2"><Button variant={indices[1]}>Termos</Button></div>
-                        <div className="p-2"><Button variant={indices[2]}>Validação</Button></div>
-                        <div className="p-2"><Button variant={indices[3]}>Relato</Button></div>
-                        <div className="p-2"><Button variant={indices[4]}>Confirmação</Button></div>
-                        </Stack>
+                        <div className={`p-2 etapa text-primary`}>
+                            <TiDocumentText />
+                            <p>Termos</p>
+                        </div>
+                        <div className={`p-2 etapa text-${indice > 1 ? "primary" : "secondary"}`}>
+                            <FaListCheck />
+                            <p>Validação</p>
+                        </div>
+                        <div className={`p-2 etapa text-${indice > 2 ? "primary" : "secondary"}`}>
+                            <RiSpeakLine />
+                            <p>Relato</p>
+                        </div>
+                        <div className={`p-2 etapa text-${indice > 3 ? "primary" : "secondary"}`}>
+                            <FaRegThumbsUp />
+                            <p>Confirmação</p>
+                        </div>
                     </div>
                 </Card.Body>
             </Card>
